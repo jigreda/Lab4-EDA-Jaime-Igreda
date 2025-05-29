e2cd14c696046056210d3be8fc3c79a78462c4b3
jigreda <jigreda@unsa.edu.pe>
2025-05-29 16:07:00 -0500

    Implementando el metodo para crear la lista
    Este metodo permite crear una lista circular, aun no esta probado

--- EjercicioProp2.java
+++ EjercicioProp2.java
@@ -9,5 +9,16 @@ public class ListaCircular<T> {
     }
   }
   public Node<T> crearListaCircular(T[] datos) {
-    
+    if(datos==null||datos.length==0)
+      return null;
+    Node<T> cabeza=new Node<>(datos[0]); //Primer nodo
+    Node<T> actual=cabeza;
+    for(int i=1;i<datos.length;i++) {
+      Node<T> nuevo=new Node<>(datos[i]);
+      actual.next=nuevo;
+      actual=nuevo;
+    }
+    actual.next=cabeza; //Hacer la lista circular
+      return cabeza;
   }
+}
