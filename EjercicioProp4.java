1378f292686ea8cd6c6dff44acae53b0a2a199ff
jigreda <jigreda@unsa.edu.pe>
2025-05-29 17:23:21 -0500

    Imprimiendo la lista circular de 2 formas
    Imprimiendo la lista con un system.out.print y con un bucle while

--- EjercicioProp4.java
+++ EjercicioProp4.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 public class ListaCircularUtil {
   public static void main(String[] args) {
     //Crear una lista enlazada (LinkedList)
@@ -7,5 +8,20 @@ public class ListaCircularUtil {
     for(int i=1;i<=12;i++){
       lista.add(i);
     }
+    //Imprimir la lista circular (una vuelta completa)
+    System.out.println("Lista circular:");
+    System.out.println(lista);
+    ListIterator<Integer> it=lista.listIterator(); //Iterador de la lista
+    int contador=0, tam=lista.size();
+    while(contador<tam){ //Recorremos exactamente una vuelta
+      if(it.hasNext()){
+        System.out.print(it.next()+" ");
+      }else{
+        it=lista.listIterator(); //Reinicio del iterador al inicio (simula circularidad)
+        System.out.print(it.next()+" ");
+      }
+      contador++;
+    }
+    System.out.println();
   }
 }
