af2624d5ae5b4ef1f202f765738ed111c54061c7
jigreda <jigreda@unsa.edu.pe>
2025-05-28 22:06:41 -0500

    Añadiendo la estructura inicial para el Ejercicio Propuesto 1
    Este ejercicio pide crear una lista doblemente enlazada usando la clase Nodo

---
+++ EjercicioProp1.java
@@ -0,0 +1,14 @@
+public class ListaDoble {
+  static class Node {
+    int data;
+    Node next;
+    Node prev;
+    Node(int d) {
+      data=d;
+      next=null;
+      prev=null;
+    }
+  }
+  public static void main(String[] args) {
+  }
+}
