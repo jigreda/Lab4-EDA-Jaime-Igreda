0d179ab21400d4c64974a286e49194e8e5be71a2
jigreda <jigreda@unsa.edu.pe>
2025-05-29 17:00:30 -0500

    Creando la lista circular con java.util
    Proximo paso mostrar la lista(probar que si se creo correctamente)

---
+++ EjercicioProp4.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+public class ListaCircularUtil {
+  public static void main(String[] args) {
+    //Crear una lista enlazada (LinkedList)
+    LinkedList<Integer> lista=new LinkedList<>();
+    //Agregar los elementos del 1 al 12
+    for(int i=1;i<=12;i++){
+      lista.add(i);
+    }
+  }
+}
